f2335ce5d9be2dd637b2ea2c6a12c0951568b6e7
mg-anjaan <mayur01garg@gmail.com>
2025-10-29 14:06:43 +0530

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,1 +1,8 @@
-# handlers package
+from aiogram import Dispatcher
+from . import group_guard, moderation, admin_tag, welcome
+
+def register_handlers(dp: Dispatcher):
+    group_guard.setup_group_guard(dp)
+    moderation.setup_moderation(dp)
+    admin_tag.setup_admin_tag(dp)
+    welcome.setup_welcome(dp)
