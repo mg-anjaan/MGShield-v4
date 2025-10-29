67db76fb1f1db117201e76d8cfe3b65e778711a5
mg-anjaan <mayur01garg@gmail.com>
2025-10-29 16:07:53 +0530

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,6 +1,14 @@
+# handlers/__init__.py
+
 from .moderation import setup_moderation
 from .group_guard import setup_group_guard
+from .admin_tag import setup_admin_tag
+from .welcome import setup_welcome
+from .filters import setup_filters  # <--- MUST BE PRESENT NOW
 
-def register_handlers(dp):
+def register_all_handlers(dp):
     setup_moderation(dp)
     setup_group_guard(dp)
+    setup_admin_tag(dp)
+    setup_welcome(dp)
+    setup_filters(dp) # <--- MUST BE PRESENT NOW
